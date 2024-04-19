d33a9bf6f731af67bd4a4438bdd5eacd642a3fba
Arya Parolia <99472244+Arya161@users.noreply.github.com>
2024-04-19 15:55:13 +0530

    First Commit

---
+++ src/App.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom"
+import Header from './Components/Header'
+import Footer from './Components/Footer'
+import Home from './Views/Home';
+import About from './Views/About';
+//import Product from './Views/Product'
+
+function App() {
+  return (
+    <div className="relative pb-10 min-h-screen">
+      <Router>
+        
+        <Header/>
+
+        <div className="p-3">
+
+        <Routes>
+
+          <Route exact path="/">
+            <Home/>
+          </Route>
+
+          <Route path="/about">
+            <About/>
+          </Route>
+          {/*
+          <Route path="/products/:id">
+            <Product/>
+          </Route>
+  */}
+
+        </Routes>
+
+        </div>
+
+        <Footer/>
+
+      </Router>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Views/Home.js
@@ -0,0 +1,27 @@
+import React from 'react'
+//import Loader from '../Components/Loader'
+//import ProductCard from '../Components/ProductCard'
+//import { useAxiosGet } from '../Hooks/HttpRequests'
+
+function Home(){
+    
+    let content = null
+
+ 
+
+    return (
+        <div className="container mx-auto">
+            <h1 className="font-bold text-2xl mb-3">
+                Best Sellers
+            </h1>
+            <div className="md:flex flex-wrap md:-mx-3">
+               <p>
+                Hi
+               </p>
+            </div>
+        </div>
+    )
+
+}
+
+export default Home
